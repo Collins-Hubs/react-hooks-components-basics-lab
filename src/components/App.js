c45901c09f73184c03028bd9f43b7a8f7bf5d2fd
Collins <collinskibet7545@gmail.com>
2024-08-09 14:33:28 +0300

    Update

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 
 function NavBar() {
@@ -16,15 +17,19 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+function About() {
+    return (
+        <h1>About</h1>
+    );
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
